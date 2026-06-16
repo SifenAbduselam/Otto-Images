53eddd292b7c475fe33fd85e468c966f1d69047d
Sifen Abduselam <sifenabduselam7@gmail.com>
2026-06-16 00:31:49 -0700

    admin login created

---
+++ src/pages/AdminLogin.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { supabase } from "../lib/supabaseClient";
+
+export default function AdminLogin() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = async () => {
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) {
+      alert(error.message);
+    } else {
+      window.location.href = "/admin";
+    }
+  };
+
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Admin Login</h1>
+
+      <input
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <br /><br />
+
+      <input
+        placeholder="Password"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <br /><br />
+
+      <button onClick={login}>Login</button>
+    </div>
+  );
+}
