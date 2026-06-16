750f0aa08b120efe911fc44776c0be82529bfe3e
Sifen Abduselam <sifenabduselam7@gmail.com>
2026-06-16 05:42:43 -0700

    fixing  the sequrity issue

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,4 +1,3 @@
-// ProtectedRoute.jsx
 import { useEffect, useState } from "react";
 import { supabase } from "../lib/supabaseClient";
 import { Navigate } from "react-router-dom";
@@ -8,23 +7,29 @@ export default function ProtectedRoute({ children }) {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    // initial check
     supabase.auth.getSession().then(({ data }) => {
       setSession(data.session);
       setLoading(false);
     });
 
+    // listen to changes
     const { data: listener } = supabase.auth.onAuthStateChange(
       (_, session) => {
         setSession(session);
       }
     );
 
-    return () => listener.subscription.unsubscribe();
+    return () => {
+      listener.subscription.unsubscribe();
+    };
   }, []);
 
   if (loading) return <p>Loading...</p>;
 
-  if (!session) return <Navigate to="/admin/login" />;
+  if (!session) {
+    return <Navigate to="/admin/login" />;
+  }
 
   return children;
 }
